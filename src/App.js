24eca998f9d2ca41a95d585550edc1760b489585
vahsimonian <simonianvah@gmail.com>
2024-01-25 12:20:39 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
 import './App.css';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <nav>
+        <ul class="nav-list">
+          <li class="active">
+            <a href="index.html" aria-current="page">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="about.html">About</a>
+          </li>
+          <li>
+            <a href="blog.html">Blog</a>
+          </li>
+          <li>
+            <a href="#">Another</a>
+          </li>
+          <li>
+            <a href="#">Page</a>
+          </li>
+          <li>
+            <a href="#">And another</a>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <h1>Home</h1>
+      </main>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../index.css';
+
+const Navbar = () => {
+  return <nav></nav>;
+};
+
+export default Navbar;
