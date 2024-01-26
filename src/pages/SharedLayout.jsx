a7c42608b1ce1849774d460013ab76762326986f
vahsimonian <simonianvah@gmail.com>
2024-01-26 12:04:35 +0700

    commit

--- src/pages/SharedLayout.jsx
+++ src/pages/SharedLayout.jsx
@@ -1,26 +1,18 @@
 import React from 'react';
 import StyledNavbar from './StyledNavbar';
-import { Outlet, useLocation } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import '../App.css';
 
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
-
 const SharedLayout = () => {
-  let location = useLocation();
-
   return (
-    <TransitionGroup>
-      <CSSTransition key={location.key} classNames={'fade'} timeout={300}>
-        <div className="layout">
-          <div className="styled-navbar">
-            <StyledNavbar />
-          </div>
-          <main className="content">
-            <Outlet />
-          </main>
-        </div>
-      </CSSTransition>
-    </TransitionGroup>
+    <div className="layout">
+      <div className="styled-navbar">
+        <StyledNavbar />
+      </div>
+      <main className="content">
+        <Outlet />
+      </main>
+    </div>
   );
 };
 
