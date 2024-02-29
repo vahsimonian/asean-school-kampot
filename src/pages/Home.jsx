f10b8007fce58bd9f792900a35c76bf9952f8155
vahsimonian <simonianvah@gmail.com>
2024-02-29 19:58:20 +0700

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import homeImage from '../assets/home.jpg';
 
 const Home = () => {
   return (
     <div className="home-page">
-      <h1>Welcome to Our School</h1>
+      <h1 className="welcome">Welcome to Our School</h1>
+      <img src={homeImage} alt="home" className="home-img" />
       <p>
         This is a brief introduction to our school. Here, you can find
         information about our values, mission, and educational programs.
